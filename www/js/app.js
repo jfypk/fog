0de941cc1f59348511d5e58fe119afc9c8f07dba
Jeffrey Park <jypark03@gmail.com>
2017-02-13 11:52:46 -0500

    First commit

---
+++ www/js/app.js
@@ -0,0 +1,24 @@
+// Setup Lifecycle Events
+function appPause() {
+    //alert("Pause"); // This alert makes the app not work, don't alert here
+    console.log("pause");
+}
+
+function appResume() {
+    alert("Resume");
+    console.log("resume");
+}
+
+function ready() {
+    alert("Hello There");
+    document.addEventListener('resume', appResume);
+    document.addEventListener('pause', appPause);
+}
+
+function loaded() {
+    // Cordova is ready for native integration/plugins/cordova specific
+    document.addEventListener('deviceready', ready);
+}
+
+window.addEventListener('load',loaded);
+
